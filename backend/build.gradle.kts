d65898bdd82df6075dec6b95713632b4ea05bf81
Oleg Schelyklanov <olegshtch@yandex.ru>
2023-08-10 23:56:24 +0300

    Minimal application

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,10 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 description = "backend"
+
+plugins {
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral() 
 }
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
 
-plugins {
-    kotlin("jvm") version "1.9.0"
-    application
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
