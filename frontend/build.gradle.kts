8faba45832a546963e6c8df52f058c56d181a12c
Oleg Schelyklanov <olegshtch@yandex.ru>
2023-08-11 22:09:48 +0300

    Fill fronend subproject

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,1 +1,15 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
 description = "frontend"
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js(IR).browser()
+}
