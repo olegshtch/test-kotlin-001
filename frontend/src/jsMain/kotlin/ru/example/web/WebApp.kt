e26ab887833b3abe78f804fb0b02d5e382249795
Oleg Schelyklanov <olegshtch@yandex.ru>
2023-08-30 22:09:54 +0300

    Read hello from backend

--- frontend/src/jsMain/kotlin/ru/example/web/WebApp.kt
+++ frontend/src/jsMain/kotlin/ru/example/web/WebApp.kt
@@ -1,16 +1,19 @@
 package ru.example.web
 
 import dev.fritz2.core.*
+import dev.fritz2.remote.http
 import kotlinx.coroutines.flow.map
 
 fun main() {
     val store: Store<String> = storeOf("Hello")
+
+    val httpApi = http().acceptJson().contentType("application/json")
 
     render() {
         h1 { +"Test" }
         p {
             store.data.render { value -> +value }
         }
-        Window.loads.map { _ -> "Hello2" } handledBy store.update
+        Window.loads.map { _ -> httpApi.get("/hello").body()} handledBy store.update
     }
 }
