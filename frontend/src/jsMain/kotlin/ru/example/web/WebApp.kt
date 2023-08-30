90565391ab4f8025e679d50b8b9960f72cd62fdc
Oleg Schelyklanov <olegshtch@yandex.ru>
2023-08-30 21:10:07 +0300

    Add initial store

--- frontend/src/jsMain/kotlin/ru/example/web/WebApp.kt
+++ frontend/src/jsMain/kotlin/ru/example/web/WebApp.kt
@@ -1,9 +1,16 @@
 package ru.example.web
 
+import dev.fritz2.core.Store
 import dev.fritz2.core.render
+import dev.fritz2.core.storeOf
 
 fun main() {
+    val store: Store<String> = storeOf("Hello")
+
     render() {
         h1 { +"Test" }
+        p {
+            store.data.render { value -> +value }
+        }
     }
 }
